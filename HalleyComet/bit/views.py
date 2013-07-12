1fae757ef672d69d81080bf9d53e5de924f00dd2
cpyou <745690939@qq.com>
2013-07-12 10:52:57 +0800

    test

--- HalleyComet/bit/views.py
+++ HalleyComet/bit/views.py
@@ -1,5 +1,22 @@
 from django.http import HttpResponse,HttpResponseRedirect
-
+from django.shortcuts import render_to_response
+from django import forms
+class UserForm(forms.Form):
+    long_url = forms.CharField()
+	short_url = forms.CharField()
 def index(req):
-	return HttpResponse("hello world,welcomle to our domain")
+
+    if req.method == 'POST':
+	    uf = UserForm(req.POST)
+		if uf.is_valid():
+		    long_url = uf.cleaned_data['short_url']
+			short_url = hashlib.sha1(long_url).hexdigest()
+			short_url = short_url[:8]
+			return render_to_response('index.html',{'uf':uf,})
+
+	return render_to_response("index.html",{'uf':uf})
+ 
+
+
+    
 
