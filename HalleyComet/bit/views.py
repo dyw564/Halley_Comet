7a8a538c61bd21ddc8e995363aa6e464858d34ee
xiguazao <wei286069122@163.com>
2013-07-16 16:26:03 +0800

    Update views.py

--- HalleyComet/bit/views.py
+++ HalleyComet/bit/views.py
@@ -5,6 +5,8 @@ from django import forms
 from bit.models import Url
 import hashlib
 
+
+
 class UrlForm(forms.Form):
     long_url = forms.CharField(max_length = 200)
 
@@ -14,18 +16,17 @@ def index(req):
         if lu.is_valid():
             long_url = lu.cleaned_data["long_url"]
             longx_url = hashlib.md5(long_url).hexdigest()
-            short2_url = longx_url[0:8]
-            user = Url.objects.filter(long_url__exact = long_url)
-            if user:
+            short_url = longx_url[0:8]
+            db_url = Url.objects.filter(long_url__exact = long_url)
+            if db_url:
                 full_long_url= user[0].long_url
                 if full_long_url[0:4] == "http":
                     return HttpResponseRedirect(full_long_url)
                 else:
                     return HttpResponseRedirect("http://%s"%full_long_url)
             else:
-                url= Url.objects.create(long_url=long_url,short_url=short2_url)
-                print url
-                return HttpResponse("short_url:%s:and:%s"%(url,short2_url))
+                url= Url.objects.create(long_url=long_url,short_url=short_url)
+                return HttpResponse("short_url:%s:and:%s"%(url,short_url))
     else:
         lu = UrlForm()
     return render_to_response('index.html',{'lu':lu})
