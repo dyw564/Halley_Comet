43bb2a5adb2f171bf45fef970f27ef8a07856431
xiguazao <wei286069122@163.com>
2013-07-24 11:08:56 +0800

    views格式pep

--- HalleyComet/bit/views.py
+++ HalleyComet/bit/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render_to_response
 from django import forms
 from bit.models import Url
 import hashlib
-from bit.def_url import judgment,shortTo,shortToLong
+from bit.def_url import judgment, shortTo, shortToLong
 
 class UrlForm(forms.Form):
     long_url = forms.CharField(max_length=200)
@@ -16,16 +16,16 @@ def index(req):
             #判断长url是否有http://
             #judgment(lu)
             long_url = judgment(lu)             
-            db_url = Url.objects.filter(long_url__exact = long_url)
+            db_url = Url.objects.filter(long_url__exact=long_url)
             if db_url:
 		        short_url = db_url[0].short_url
             else:
                 short_url = shortTo(long_url)
-            return render_to_response('index.html',{'lu': lu, 'short_url': short_url, 'long_url': long_url})
+            return render_to_response('index.html', {'lu':lu, 'short_url':short_url, 'long_url':long_url})
     else:
         lu = UrlForm()
-    return render_to_response('index.html',{'lu': lu})
+    return render_to_response('index.html', {'lu':lu})
 
-def turn(req,short_hash):
+def turn(req, short_hash):
     full_long_url = shortToLong(short_hash)
     return HttpResponseRedirect(full_long_url)
