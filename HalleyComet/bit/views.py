ebc4577ff4c38ab49b3617d6f9a5fc4ef34b22a9
xiguazao <wei286069122@163.com>
2013-07-24 10:31:10 +0800

    10-62进制；views函数提出

--- HalleyComet/bit/views.py
+++ HalleyComet/bit/views.py
@@ -1,8 +1,10 @@
+#-*-coding:utf8-*-
 from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render_to_response 
 from django import forms
 from bit.models import Url
 import hashlib
+from bit.def_url import judgment,shortTo,shortToLong
 
 class UrlForm(forms.Form):
     long_url = forms.CharField(max_length=200)
@@ -11,25 +13,19 @@ def index(req):
     if req.method == "POST":
         lu = UrlForm(req.POST)
         if lu.is_valid():
-            long_url = lu.cleaned_data["long_url"]
+            #判断长url是否有http://
+            #judgment(lu)
+            long_url = judgment(lu)             
             db_url = Url.objects.filter(long_url__exact = long_url)
             if db_url:
-		short_url = db_url[0].short_url
+		        short_url = db_url[0].short_url
             else:
-                longx_url = hashlib.md5(long_url).hexdigest()
-                short_url = "localhost:8000/%s"%longx_url[0:8]
-                url= Url.objects.create(long_url = long_url,short_url = short_url)
-                if long_url[:4] != 'http':
-                    long_url = 'http://'+long_url
+                short_url = shortTo(long_url)
             return render_to_response('index.html',{'lu': lu, 'short_url': short_url, 'long_url': long_url})
     else:
         lu = UrlForm()
     return render_to_response('index.html',{'lu': lu})
 
 def turn(req,short_hash):
-    short = "localhost:8000/%s" % short_hash
-    long_url = Url.objects.filter(short_url__exact = short)
-    full_long_url= long_url[0].long_url
-    if full_long_url[0:4] != "http":
-    	full_long_url = "http://"+full_long_url
+    full_long_url = shortToLong(short_hash)
     return HttpResponseRedirect(full_long_url)
